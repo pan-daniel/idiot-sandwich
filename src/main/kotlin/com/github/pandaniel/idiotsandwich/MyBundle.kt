fa28bee7bdefb02a18d455fe679545abf6aad14c
GitHub Action <action@github.com>
2021-06-09 18:16:51 +0000

    Template cleanup

--- src/main/kotlin/com/github/pandaniel/idiotsandwich/MyBundle.kt
+++ src/main/kotlin/com/github/pandaniel/idiotsandwich/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.pandaniel.idiotsandwich
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/pandaniel/idiotsandwich/services/MyProjectService.kt
+++ src/main/kotlin/com/github/pandaniel/idiotsandwich/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.pandaniel.idiotsandwich.services
 
+import com.github.pandaniel.idiotsandwich.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
